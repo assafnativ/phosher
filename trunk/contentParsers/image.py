77a9b7113859a7a70b0b1912c8e2c533eb87d6f7
nativ.assaf@gmail.com <nativ.assaf@gmail.com@6ccd26c7-4963-ed8c-94d4-dc995456032a>
2014-09-25 13:11:39 +0000

    Added display of tree of Image file. Added image files BB5 padding support.

--- trunk/contentParsers/image.py
+++ trunk/contentParsers/image.py
@@ -18,6 +18,7 @@ def main():
     userOptions.add_option("-u", "--dumpDest",  dest="dumpDest",    type="string", help="Where to dump files from image")
     userOptions.add_option("-c", "--createIma", dest="createIma",   action="store_true", help="Create FAT16 image file")
     userOptions.add_option("-w", "--imageName", dest="imageName",   type="string", help="Destination FAT16 image file")
+    userOptions.add_option("-l", "--list",      dest="listFiles",   action="store_true", help="Display list of all files in image as tree")
     userOptions.add_option("-v", "--verbose",   dest="isVerbose",   action="store_true", help="Set verbose output on")
     userOptions.set_defaults(inputFile=None, outputFile=None, isVerbose=False)
     (options, args) = userOptions.parse_args(sys.argv[1:])
@@ -28,10 +29,15 @@ def main():
     dumpDest    = options.dumpDest
     createIma   = options.createIma
     imageName   = options.imageName
+    listFiles   = options.listFiles
     isVerbose   = options.isVerbose
     PATCHES, patchesDefines = loadPatchesFromFile(patchesFile, userOptions, isVerbose, globs=patcher)
     inputFile = cmdLineInputFile(patchesDefines, inputFile, userOptions)
     outputFile = cmdLineOutputFile(patchesDefines, outputFile, inputFile)
+    if None == dumpDest:
+        writeOutput = False
+    else:
+        writeOutput = True
     if "DUMP_DEST" not in patchesDefines and None == dumpDest:
         dumpDest = inputFile + '_DUMP'
     elif "DUMP_DEST"   in patchesDefines and None == dumpDest:
@@ -46,6 +52,7 @@ def main():
     fat16 = parseFAT16(imageData, isVerbose=isVerbose)
 
     if None != patchesFile:
+        writeOutput = True
         fat16Patcher = patcher.Fat16Patcher(fat16)
         fat16Patcher.patch(PATCHES)
     if isDump:
@@ -53,8 +60,11 @@ def main():
     if createIma:
         imageData = fat16.makeNoPadding()
         file(imageName, 'wb').write(imageData)
-    nokiaFile.plain = fat16.make()
-    file(outputFile, 'wb').write(nokiaFile.encode())
+    if listFiles:
+        fat16.displayTree()
+    if writeOutput:
+        nokiaFile.plain = fat16.make()
+        file(outputFile, 'wb').write(nokiaFile.encode())
 
 if __name__ == "__main__":
     main()
